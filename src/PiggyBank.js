020bc1014dfc4edb6540546e99e0bfa23f587be9
Marc Rayner <26255479+marcjrayner@users.noreply.github.com>
2020-01-27 11:45:20 +0000

    adds constructor bind for deposit button and function

--- src/PiggyBank.js
+++ src/PiggyBank.js
@@ -1,9 +1,28 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 
 class PiggyBank extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            total: 0
+        }
+
+        this.deposit = this.deposit.bind(this)
+    }
+
+    deposit() {
+        this.setState(previousState => {
+            return {total: previousState.total + 5}
+        })
+    }
+
     render() {
         return (
-        <h1>{this.props.title}</h1>      
+            <Fragment>
+            <h1>{this.props.title}</h1> 
+            <p>Total: £{this.state.total}</p>
+            <button onClick={this.deposit}>Deposit</button>
+            </Fragment>
         )
     }
 }
